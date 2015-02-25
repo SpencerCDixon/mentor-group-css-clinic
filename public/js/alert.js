9f5665b0ab648751ab482f417396497070b99d63
Spencer Dixon <spencercdixon@gmail.com>
2015-02-25 10:35:38 -0500

    Added JS functionality to the website

---
+++ public/js/alert.js
@@ -0,0 +1,18 @@
+$(function() {
+  var $addContentButton = $('#add-content');
+
+  $addContentButton.on('click', function() {
+    var info = prompt('What do you want to add to page?');
+    var $contentDiv = $('<div>').attr('class', 'dynamic-content');
+
+    $('#dynamic-content').append($contentDiv.text(info));
+  });
+
+});
+
+.hidden {
+  display: none;
+}
+
+
+
